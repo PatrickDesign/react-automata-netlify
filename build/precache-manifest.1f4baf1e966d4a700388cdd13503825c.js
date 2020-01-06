c03bc038cf369b0b34457871dded0eba7c34a401
PatrickW <Patrickwees@gmail.com>
2020-01-06 13:36:09 -0800

    testing netlify deployment

---
+++ build/precache-manifest.1f4baf1e966d4a700388cdd13503825c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87034704a50d8807b59dd2cfa4ad1f31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3652dfde4fbc3723fb5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cbdfeccc32716e3675c6",
+    "url": "/static/js/2.5126c6a3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5126c6a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3652dfde4fbc3723fb5",
+    "url": "/static/js/main.c4ecac89.chunk.js"
+  },
+  {
+    "revision": "638c548a6fa56d30f818",
+    "url": "/static/js/runtime-main.9336bc65.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
